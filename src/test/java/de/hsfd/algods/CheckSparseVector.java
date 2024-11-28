3fd4514aadac3139c3b5bebdb0f3ed6b4e9f45b5
Jack Nguyen <nhintngoc@gmail.com>
2024-11-28 13:37:55 +0100

    trial commit to a new branch

--- src/test/java/de/hsfd/algods/CheckSparseVector.java
+++ src/test/java/de/hsfd/algods/CheckSparseVector.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class CheckSparseVector {
+    
     @Test
     void checkCountEqualsLength(){
         SparseVector v = new SparseVector(7);
